fb5826bb2f8675b11bd137a1cc460236b9f5be66
phillipphoenix <pphoelich@gmail.com>
2015-02-08 06:11:27 +0100

    Main Menu added to the game with a title screen and a text saying to press a button. Then we Fade into the game.

---
+++ Assets/Scripts/WhiteBoxController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhiteBoxController : MonoBehaviour {
+
+    private float alpha = 1f;
+    public SpriteRenderer whiteSprite;
+
+    void Awake()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        StartCoroutine(FadeOut());
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator FadeOut()
+    {
+        while (alpha > 0f)
+        {
+            alpha += -0.05f;
+            whiteSprite.color = new Color(whiteSprite.color.r, whiteSprite.color.g, whiteSprite.color.b, alpha);
+            yield return new WaitForSeconds(0.1f);
+        }
+        Destroy(this);
+    }
+}
